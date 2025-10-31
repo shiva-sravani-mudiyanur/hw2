f92d8a84ac59b560682e2c8638efe529fc8ca783
shiva-sravani-mudiyanur <shivasravani@umass.edu>
2025-10-31 16:29:23 -0400

    Updated AmoutFilter (such that it uses comparator )

--- expense_tracker/src/model/AmountFilter.java
+++ expense_tracker/src/model/AmountFilter.java
@@ -4,19 +4,48 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /*
- * To filter the list of transactions based on the specific amount entered.
+ * To filter the list of transactions based on the comparator selected and amount entered.
  */
 public class AmountFilter implements TransactionFilter {
     private final double amount;
+    private String comparator;
 
-    public AmountFilter(double amount) {
+
+    public AmountFilter(double amount, String comparator) {
         this.amount = amount;
+        this.comparator = comparator;
     }
 
     @Override
     public List<Transaction> filter(List<Transaction> transactions) {
-        return transactions.stream()
-                           .filter(t -> t.getAmount() == amount)
-                           .collect(Collectors.toList());
+        switch (this.comparator) {
+            case "=":
+                return transactions.stream()
+                                   .filter(t -> t.getAmount() == this.amount)
+                                   .collect(Collectors.toList());
+                                
+            case "<":
+                return transactions.stream()
+                                   .filter(t -> t.getAmount() < this.amount)
+                                   .collect(Collectors.toList());
+
+            case "<=":
+                return transactions.stream()
+                                   .filter(t -> t.getAmount() <= this.amount)
+                                   .collect(Collectors.toList());
+
+            case ">":
+                return transactions.stream()
+                                   .filter(t -> t.getAmount() > this.amount)
+                                   .collect(Collectors.toList());
+            
+            case ">=":
+                return transactions.stream()
+                                   .filter(t -> t.getAmount() >= this.amount)
+                                   .collect(Collectors.toList());
+            
+            default:
+                return transactions;
+        }
     }
 }
